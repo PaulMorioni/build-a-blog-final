5745afc8044048411d619c9c7fc943d231887c13
PaulMorioni <44041460+PaulMorioni@users.noreply.github.com>
2019-02-13 19:34:22 -0600

    Fixed values populating form if one element is missing input

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:1234@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGc7Fgh45'
 
 
 class Blog(db.Model):
@@ -27,15 +28,32 @@ def index():
 
 @app.route('/new_entry', methods = ['POST','GET'])
 def new_entry():
+
+    post_name = ''
+    post_body = ''
+
     if request.method == 'POST':
         post_name = request.form.get('blog_title')
         post_body = request.form.get('blog_body')
-        new_post = Blog(post_name, post_body)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/')
+
+        if post_name:
+
+            if post_body:
+                new_post = Blog(post_name, post_body)
+                db.session.add(new_post)
+                db.session.commit()
+                post_id = new_post.id
+                post_id_str=str(post_id)
+                url = '/post?id=' + post_id_str
+                return redirect(url)
+            else:
+                flash('Body Required')
+        else:
+            if post_body == "":
+                flash('Body Required')
+            flash('Title Required')
     
-    return render_template("add_post.html", title="New Post")
+    return render_template("add_post.html", title="New Post", post_name = post_name , post_body = post_body)
 
 @app.route('/post', methods=['POST','GET'])   
 def post_page():
